94635b31d053a4f2ab41acbd097910d43088fc92
Santiago Flores <santiagojesuslaureano369@gmail.com>
2026-04-20 21:25:08 -0600

    feat: Modelos creados

---
+++ app/Models/Post.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'title',
+        'slug',
+        'content',
+        'image_url',
+        'read_time',
+        'category',
+        'is_published',
+    ];
+}
